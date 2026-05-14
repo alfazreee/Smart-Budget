aa2e40fe3be9647ed86781d6a69d855d72872397
Mahesa Revanenda Al Fajri <alfajrief@gmail.com>
2026-05-14 14:54:24 +0700

    Membuat card dan linear progres

--- app/src/main/java/com/mahesa0004/smartbudget/MainActivity.kt
+++ app/src/main/java/com/mahesa0004/smartbudget/MainActivity.kt
@@ -7,11 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,6 +32,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +41,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mahesa0004.smartbudget.ui.theme.SmartBudgetTheme
+import androidx.compose.ui.graphics.StrokeCap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,7 +78,7 @@ fun MainScreen() {
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize().padding(80.dp),
+        modifier = modifier.fillMaxSize().padding(32.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -74,11 +89,96 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         )
         Text(
             text = "Smart Budget, Simple Life.",
-            fontWeight = FontWeight.Medium,
+            fontSize = 20.sp,
             modifier = Modifier.padding(top = 16.dp)
         )
+        BiayaBulananCard()
     }
 }
+
+@Composable
+fun BiayaBulananCard() {
+
+    val spent = 600000.0
+    val budget = 1500000.0
+    val progress = (spent / budget).toFloat().coerceIn(0f, 1f)
+    val date = "11-05-2026"
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFB3C6F5))
+    ) {
+
+        Column(
+            modifier = Modifier.padding(18.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = "Biaya bulanan",
+                    color = Color.Black,
+                    modifier = Modifier.padding(2.dp)
+                )
+                Text(
+                    text = date,
+                    color = Color.Black,
+                    modifier = Modifier.padding(2.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp)
+                    .clip(RoundedCornerShape(50))
+            ){
+
+
+                LinearProgressIndicator(
+                    progress = { progress },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(32.dp)
+                        .clip(RoundedCornerShape(50)),
+                    color = Color(0xFF3D4F6B),
+                    trackColor = Color(0xFFE0E0E0),
+                    strokeCap = StrokeCap.Butt
+
+                )
+                Text(
+                    text = formatRupiah(spent),
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 12.dp)
+                )
+                Text(
+                    text = formatRupiah(budget),
+                    color = Color.Black,
+                    fontSize = 12.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 12.dp)
+                )
+            }
+        }
+    }
+}
+
+fun formatRupiah(mount: Double) : String {
+    val formatted = String.format("%,.0f", mount).replace(',','.')
+    return "Rp$formatted"
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
